3d8f24346a00451b9c7c92b160cc1123857d2344
delucis <swithinbank@gmail.com>
2018-04-20 13:47:54 +0200

    Update temporary filenames in tests to use .glyphs extension.

--- test.js
+++ test.js
@@ -5,25 +5,25 @@ import tempfile from 'tempfile'
 import writeGlyphs from '.'
 
 test('async', async t => {
-  const tmp = path.join(tempfile(), 'foo')
+  const tmp = path.join(tempfile(), 'foo.glyphs')
   await writeGlyphs(tmp, {foo: true})
   t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = true;\n}\n')
 })
 
 test('sync', t => {
-  const tmp = path.join(tempfile(), 'foo')
+  const tmp = path.join(tempfile(), 'foo.glyphs')
   writeGlyphs.sync(tmp, {foo: true}, {detectIndent: true, indent: 2})
   t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = true;\n}\n')
 })
 
 test('write arrays', async t => {
-  const tmp = path.join(tempfile(), 'foo')
+  const tmp = path.join(tempfile(), 'foo.glyphs')
   await writeGlyphs(tmp, {foo: [1, 5]})
   t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = (\n1,\n5\n);\n}\n')
 })
 
 test('write objects', async t => {
-  const tmp = path.join(tempfile(), 'foo')
+  const tmp = path.join(tempfile(), 'foo.glyphs')
   await writeGlyphs(tmp, {foo: {}, bar: {baz: []}})
   t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = {\n};\nbar = {\nbaz = (\n\n);\n};\n}\n')
 })
