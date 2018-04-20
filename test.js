ca3729b7dac82697c9d249a67bc213327ca76843
delucis <swithinbank@gmail.com>
2018-04-20 14:26:41 +0200

    Rewrite tests to use more font file-specific test strings.

--- test.js
+++ test.js
@@ -6,24 +6,34 @@ import writeGlyphs from '.'
 
 test('async', async t => {
   const tmp = path.join(tempfile(), 'foo.glyphs')
-  await writeGlyphs(tmp, {foo: true})
-  t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = true;\n}\n')
+  const data = { familyName: 'My Font' }
+  await writeGlyphs(tmp, data)
+  t.is(fs.readFileSync(tmp, 'utf8'), '{\nfamilyName = "My Font";\n}\n')
 })
 
 test('sync', t => {
   const tmp = path.join(tempfile(), 'foo.glyphs')
-  writeGlyphs.sync(tmp, {foo: true}, {detectIndent: true, indent: 2})
-  t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = true;\n}\n')
+  const data = { familyName: 'My Font' }
+  writeGlyphs.sync(tmp, data)
+  t.is(fs.readFileSync(tmp, 'utf8'), '{\nfamilyName = "My Font";\n}\n')
 })
 
 test('write arrays', async t => {
   const tmp = path.join(tempfile(), 'foo.glyphs')
-  await writeGlyphs(tmp, {foo: [1, 5]})
-  t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = (\n1,\n5\n);\n}\n')
+  const data = { DisplayStrings: [ 'handgloves', '9' ] }
+  await writeGlyphs(tmp, data)
+  t.is(fs.readFileSync(tmp, 'utf8'), '{\nDisplayStrings = (\nhandgloves,\n9\n);\n}\n')
 })
 
 test('write objects', async t => {
   const tmp = path.join(tempfile(), 'foo.glyphs')
-  await writeGlyphs(tmp, {foo: {}, bar: {baz: []}})
-  t.is(fs.readFileSync(tmp, 'utf8'), '{\nfoo = {\n};\nbar = {\nbaz = (\n\n);\n};\n}\n')
+  const data = {
+    kerning: {
+      'F9005791-A': {
+        T: { '@MMK_R_A': '-30' }
+      }
+    }
+  }
+  await writeGlyphs(tmp, data)
+  t.is(fs.readFileSync(tmp, 'utf8'), '{\nkerning = {\n"F9005791-A" = {\nT = {\n"@MMK_R_A" = "-30";\n};\n};\n};\n}\n')
 })
