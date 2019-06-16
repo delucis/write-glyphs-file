3f99644f30e7784339a7f3b019772d8208e458fd
delucis <swithinbank@gmail.com>
2019-06-16 23:06:09 +0200

    test: Add tests for bad parameters and non-`.glyphs` file extensions
    Get test coverage up to 100%

--- test.js
+++ test.js
@@ -37,3 +37,28 @@ test('write objects', async t => {
   await writeGlyphs(tmp, data)
   t.is(fs.readFileSync(tmp, 'utf8'), '{\nkerning = {\n"F9005791-A" = {\nT = {\n"@MMK_R_A" = "-30";\n};\n};\n};\n}\n')
 })
+
+test('throws if empty file path (async)', async t => {
+  await t.throwsAsync(async () => writeGlyphs(''))
+})
+
+test('throws if empty file path (sync)', t => {
+  t.throws(() => writeGlyphs.sync(''))
+})
+
+test('throws if no data provided (async)', async t => {
+  const tmp = path.join(tempfile(), 'foo.glyphs')
+  await t.throwsAsync(async () => writeGlyphs(tmp))
+})
+
+test('throws if no data provided (sync)', t => {
+  const tmp = path.join(tempfile(), 'foo.glyphs')
+  t.throws(() => writeGlyphs.sync(tmp))
+})
+
+test('will enfore a `.glyphs` extension', async t => {
+  const tmp = path.join(tempfile(), 'foo.jpeg')
+  const data = { familyName: 'My Font' }
+  await writeGlyphs(tmp, data)
+  t.is(fs.readFileSync(`${tmp}.glyphs`, 'utf8'), '{\nfamilyName = "My Font";\n}\n')
+})
